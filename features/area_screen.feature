3d74d769d15f68a7e37f1a87867caab71142bd38
paul <chrome8282@gmail.com>
2019-08-08 23:46:47 +0300

    1

---
+++ features/area_screen.feature
@@ -0,0 +1,15 @@
+Feature: User is able to convert area units
+
+  Scenario: User is able to dismiss help
+    Given I land on help popup
+    When I click on Got it button
+    Then I land on Area screen
+
+
+  Scenario: User is able to swap values
+    Given I click on Got it button
+    And I see "Sq Kilometre" in From header
+    And I see "Sq Metre" in To Header
+    When I click on Swap button
+    Then I see "Sq Metre" in From header
+    And I see "Sq Kilometre" in To Header
---
+++ features/step_definitions/area screen steps.rb
@@ -0,0 +1,23 @@
+Given(/^I land on help popup$/) do
+  puts('User lands on help popup')
+end
+
+When(/^I click on Got it button$/) do
+  puts('User clicks on Got it button')
+end
+
+Then(/^I land on Area screen$/) do
+  puts('User lands on Area screen')
+end
+
+When(/^I click on Swap button$/) do
+  puts('User clicks Swap button')
+end
+
+Then(/^I see "([^"]*)" in From header$/) do |value|
+  puts('From header value is ' + value)
+end
+
+And(/^I see "([^"]*)" in To Header$/) do |value|
+  puts("To header value is #{value}")
+end

793afc28fd1d7afad0f32504ac0ec9726e4555c8
paul <chrome8282@gmail.com>
2019-08-11 23:54:04 +0300

    2 tests

--- features/step_definitions/area screen steps.rb
+++ features/step_definitions/area screen steps.rb
@@ -1,25 +1,29 @@
 Given(/^I land on help popup$/) do
-  puts('User lands on help popup')
+    text("Help")
 end
 
 When(/^I click on Got it button$/) do
-  puts('User clicks on Got it button')
+    find_element(id:"button1").click
 end
 
 Then(/^I land on Area screen$/) do
-  puts('User lands on Area screen')
+  text("Area")
 end
 
 When(/^I click on Swap button$/) do
-  puts('User clicks Swap button')
+  find_element(id:"fab").click
 end
 
 Then(/^I see "([^"]*)" in From header$/) do |value|
-  puts('From header value is ' + value)
+  actual_value = find_element(id:"header_text_unit_from").text
+  puts("Exepted value is #{value}")
+  puts("Actual value is #{actual_value}")
 end
 
 And(/^I see "([^"]*)" in To Header$/) do |value|
-  puts("To header value is #{value}")
+  actual_value = find_element(id:"header_text_unit_to").text
+  puts("Exepted value is #{value}")
+  puts("Actual value is #{actual_value}")
 end
 
 

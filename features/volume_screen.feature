5a1f20cb4f79364fbe112e4eeceb91b869ab4444
paul <chrome8282@gmail.com>
2019-08-12 18:45:03 +0300

    swipe and new feature

---
+++ features/volume_screen.feature
@@ -0,0 +1,11 @@
+Feature: User is able to convert Volume units
+
+  Background:
+    Given I click on Got it button
+    And I land on Area screen
+
+    @wip
+    Scenario: User is able to call menu by swiping
+      When I swipe in menu
+      Then I see app menu
+
---
+++ features/volume_screen_steps/rb.java
@@ -0,0 +1,7 @@
+public class rb {
+    @cucumber.api.java.en.When("^I swipe in menu$")
+    public void iSwipeInMenu() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+}
